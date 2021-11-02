a92c79b0ea28348ef99fc3c85a6f77ea092e7e0d
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-02 16:48:11 +0100

    create-user-db

---
+++ src/FIrebase/Credentials.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJoZmqcZP29uEK3luc3UoXak-sRpHNJLg",
+  authDomain: "crud-25edf.firebaseapp.com",
+  projectId: "crud-25edf",
+  storageBucket: "crud-25edf.appspot.com",
+  messagingSenderId: "164132278383",
+  appId: "1:164132278383:web:30acc3e22ad0de9abebd9a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
