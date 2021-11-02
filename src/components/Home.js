8b82e42dbfa913819fec34b6c6643e61dbb229b4
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-02 21:21:18 +0100

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,12 @@ import TaskList from './TaskList';
 import AddTask from './AddTask';
 
 const Home = () => {
+    const fakeData = [
+        {id:645487694564565465, descripcion:'tarea falsa 1', url:'https://picsum.photos/'},
+        {id:64544354684534545, descripcion:'tarea falsa 2', url:'https://picsum.photos/'},
+        {id:645546787867558777445, descripcion:'tarea falsa 3', url:'https://picsum.photos/'},
+        {id:6454243783683563586358/67/87534545, descripcion:'tarea falsa 4', url:'https://picsum.photos/'}
+    ]
     return (
         <Container>
             <h1>Sesion iniciada</h1>
@@ -13,7 +19,7 @@ const Home = () => {
                 onClick= {() => signOut(auth) }
             >Cerrar sesion</Button>
             <br/>
-            {/* {<TaskList />} */}
+            <TaskList  fakeData={fakeData}/>
             <AddTask/>
         </Container>
     )
