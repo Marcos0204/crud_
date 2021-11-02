c2b9e3cc3b8d977b4194fa425f2c6165133ee1d9
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-02 15:31:37 +0100

    update-config-initial

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Home from './components/Home';
+import Logueo from './components/Logueo';
+
+
+const App = () => {
+
+  const [ userGlobal, setUserGlobal ] = useState(true);
+
+
+  //funcion que se ejecutar al detectar autentificacion
+
+  return (
+    <div>
+      {userGlobal ? <Home email={userGlobal.email}/> : <Logueo/>}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Logueo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logueo = () => {
+    return (
+        <div>
+            <h1>logueo</h1>
+        </div>
+    )
+}
+
+export default Logueo
